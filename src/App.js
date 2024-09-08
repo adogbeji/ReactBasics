907688ba815e69217d627ea25f2b9fcdbd754c49
adogbeji <baka_1993@yahoo.co.uk>
2024-09-08 17:24:17 +0100

    Add more HTML & start using SASS for form styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import './App.scss';
 
+import './components/dummyForm.scss';
+
 function App() {
   return (
     <div className="App">
       <h1>Learning React</h1>
 
-      <form>
-        <input type="text"/>
-        <input type="password"/>
-        <button>Submit</button>
-      </form>
+      <div className="form-box">
+        {/* Form Start  */}
+        <form className="form">
+          <div className="form__group">
+            <input type="text" className="form__control"/>
+          </div>
+
+          <div className="form__group">
+            <input type="password" className="form__control"/>
+          </div>
+
+          {/* Button Start */}
+          <button className="form__btn form__btn--lg form__btn--dark form__btn--rounded">
+            Submit
+          </button>
+          {/* Button End */}
+        </form>
+        {/* Form End */}
+      </div>
     </div>
   );
 }
